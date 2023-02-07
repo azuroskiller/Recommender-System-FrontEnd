cdd435c1d771c32f7fe84810cdec32cc4ef766b8
Hairul Wafiq <wafiqhey@gmail.com>
2023-02-08 00:26:44 +0800

    Capstone Project Front End

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/style.css'
+import { BrowserRouter as BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import ListMovies from './components/pages/ListMovies';
+import AddMovie from './components/pages/AddMovie';
+import SignUp from './components/pages/Signup';
+import Login from './components/pages/Login';
+import Dashboard from './components/pages/Dashboard';
+import ListUser from './components/pages/ListUser';
+import ViewMovie from './components/pages/ViewMovie';
+import MovieDetails from './components/pages/MovieDetails';
+import FavMovies from './components/pages/FavMovies';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <Routes>
+            
+            <Route path = "/" element = {<Login/>}> </Route>
+            <Route path = "/AddMovie" element = {<AddMovie/>}> </Route>
+            <Route path = "/SignUp" element = {<SignUp/>}> </Route>
+            <Route path = "/Login" element = {<Login/>}> </Route>
+            <Route path = "/Dashboard" element = {<Dashboard/>}> </Route>                
+            <Route path = "/ListMovies" element={<ListMovies/>}> </Route>       
+            <Route path = "/ListUser" element={<ListUser/>}> </Route>      
+            <Route path = "/ViewMovie" element={<ViewMovie/>}> </Route> 
+            <Route path = "/MovieDetails/:id" element={<MovieDetails/>}> </Route> 
+            <Route path = "/FavMovies" element={<FavMovies/>}></Route> 
+
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
